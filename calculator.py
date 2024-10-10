bc4da71c6b23eb8e557395605659bc1d6747419b
Jessica <jb20053003@gmail.com>
2024-10-10 12:05:08 +0530

    calculator.py
    Task 2 in python programming ✌🏻

---
+++ calculator.py
@@ -0,0 +1,12 @@
+a=int(input("Enter the value for a: "))
+b=int(input("Enter the value for b: "))
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+#g=a%b
+print("The sum of",str(a),"and",str(b),"is",str(c))
+print("The difference of",str(a),"and",str(b),"is",str(d))
+print("The product of",str(a),"and",str(b),"is",str(e))
+print("The division of",str(a),"and",str(b),"is",str(f))
+#print("The modulo division of",str(a),"and",str(b),"is",str(float(g)))
